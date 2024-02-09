2a13415ff24215354d94a851ff9c1f07e0e84b46
ocircleo <salmanhossain11222626@gmail.com>
2024-02-09 22:27:24 +0600

    description and delet selected features added

--- main.js
+++ main.js
@@ -1,82 +1,112 @@
 let addButton = document.querySelector(".add");
 let input = document.querySelector(".input");
 let tasks = document.querySelector(".tasks");
-let deleteAllBtn = document.querySelector(".delete-all button");
+let deleteAllBtn = document.querySelector(".delete-all");
+let deleteSelectedBtn = document.querySelector(".delete-selected");
 let arrayOfTasks = [];
-
-if (window.localStorage.getItem("tasks")) {
-  arrayOfTasks = JSON.parse(window.localStorage.getItem("tasks"));
-  addingTasksTodiv(arrayOfTasks);
-  if (JSON.parse(window.localStorage.getItem("tasks")).length > "0") {
-    deleteAllBtn.style.display = "flex";
-  }
-}
-
-addButton.onclick = function () {
-  if (input.value !== "") {
-    if (input.value.length <= 18) {
-      addingTasksToArray(input.value);
-      deleteAllBtn.style.display = "flex";
-    } else {
-      Swal.fire({
-        title: "The task is too long!",
-        icon: "warning",
-      });
-    }
-  } else {
-    Swal.fire({
-      title: "You must write something!",
-      icon: "warning",
+let form = document.getElementById("form");
+function renderToDom() {
+  tasks.innerHTML = "";
+  if (window.localStorage.getItem("tasks")) {
+    arrayOfTasks = getLocalStorage();
+    arrayOfTasks.map((ele) => {
+      addingTasksTodiv(ele);
     });
   }
-  input.value = "";
-};
+}
+renderToDom();
+function getLocalStorage() {
+  let tasks = localStorage.getItem("tasks");
+  if (tasks) {
+    return JSON.parse(tasks);
+  } else {
+    return [];
+  }
+}
+// addButton.onclick = function () {
+//   if (input.value !== "") {
+//     if (input.value.length <= 18) {
+//       addingTasksToArray(input.value);
+//       deleteAllBtn.style.display = "flex";
+//     } else {
+//       Swal.fire({
+//         title: "The task is too long!",
+//         icon: "warning",
+//       });
+//     }
+//   } else {
+//     Swal.fire({
+//       title: "You must write something!",
+//       icon: "warning",
+//     });
+//   }
+//   input.value = "";
+// };
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let form = e.target;
+  let title, text;
+  title = form.title.value;
+  text = form.text.value;
+  addingTasksToArray(title, text);
+  form.reset();
+});
 
-function addingTasksToArray(inputValue) {
+function addingTasksToArray(title, text) {
+  let previousTasks = getLocalStorage();
   let obj = {
-    id: Date.now(),
-    title: inputValue,
+    id: previousTasks.length || 0,
+    title: title,
+    text: text,
     completed: false,
   };
-  arrayOfTasks.push(obj);
-  addingTasksTodiv(arrayOfTasks);
-  window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+  previousTasks.push(obj);
+  localStorage.setItem("tasks", JSON.stringify(previousTasks));
+  addingTasksTodiv(obj);
 }
 
-function addingTasksTodiv(allTasks) {
-  tasks.innerHTML = "";
-  allTasks.forEach((task) => {
-    let divContainer = document.createElement("div");
-    let div = document.createElement("div");
-    let checkImg = document.createElement("img");
-    div.className = "text";
-    checkImg.src = "./Images/unchecked.png";
-    checkImg.alt = "Unchecked";
-    div.appendChild(checkImg);
-    if (task.completed) {
-      divContainer.className = "checked";
-      checkImg.src = "./Images/checked.png";
-    }
-    let divText = document.createTextNode(task.title);
-    div.appendChild(divText);
-    divContainer.appendChild(div);
-    divContainer.setAttribute("taskId", task.id);
-    let delButton = document.createElement("button");
-    let delButtonText = document.createTextNode("Delete");
-    delButton.appendChild(delButtonText);
-    divContainer.appendChild(delButton);
-    delButton.addEventListener("click", function (e) {
-      divContainer.remove();
-      deletingElementFromLocStorage(
-        e.currentTarget.parentElement.getAttribute("taskId")
-      );
-      if (JSON.parse(window.localStorage.getItem("tasks")).length == "0") {
-        window.localStorage.removeItem("tasks");
-        deleteAllBtn.style.display = "none";
-      }
-    });
-    tasks.appendChild(divContainer);
+function addingTasksTodiv({ id, title, text, completed }) {
+  let divContainer = document.createElement("div");
+  divContainer.classList.add("task-container");
+  let div = document.createElement("div");
+  div.classList.add("task");
+  let titleContainer = document.createElement("div");
+  titleContainer.classList.add("title-container");
+  let checkImg = document.createElement("img");
+  checkImg.src = "./Images/unchecked.png";
+  checkImg.alt = "Unchecked";
+  checkImg.classList.add("pointer");
+  if (completed) {
+    checkImg.src = "./Images/checked.png";
+    checkImg.alt = "checked";
+  }
+  checkImg.addEventListener("click", (e) => done(e, id));
+  titleContainer.appendChild(checkImg);
+  let newTitle = document.createElement("p");
+  newTitle.textContent = title;
+  newTitle.classList.add("task-title");
+  if (completed) {
+    newTitle.classList.add("done");
+  }
+  titleContainer.appendChild(newTitle);
+  div.appendChild(titleContainer);
+  divContainer.appendChild(div);
+
+  //deleting options
+  divContainer.setAttribute("taskId", id);
+  let delButton = document.createElement("button");
+  delButton.innerText = "Delete";
+  delButton.classList.add("button");
+  delButton.classList.add("scale");
+  div.appendChild(delButton);
+  delButton.addEventListener("click", function (e) {
+    divContainer.remove();
+    deletingElementFromLocStorage(id);
   });
+  let newText = document.createElement("p");
+  newText.innerText = text || "no description";
+  divContainer.appendChild(newText);
+  tasks.appendChild(divContainer);
 }
 
 function deletingElementFromLocStorage(taskId) {
@@ -86,35 +116,42 @@ function deletingElementFromLocStorage(taskId) {
 
 deleteAllBtn.addEventListener("click", (e) => {
   window.localStorage.removeItem("tasks");
-  tasks.innerHTML = "";
-  e.target.style.display = "none";
+  renderToDom();
+});
+deleteSelectedBtn.addEventListener("click", (e) => {
+  let newTasks = getLocalStorage();
+  let filteredTasks = newTasks.filter((ele) => ele.completed != true);
+  localStorage.setItem("tasks", JSON.stringify(filteredTasks));
+  renderToDom();
 });
 
-tasks.addEventListener("click", (e) => {
-  if (
-    e.target &&
-    e.target.children &&
-    e.target.children[0] &&
-    e.target.children[0].children &&
-    e.target.children[0].children[0]
-  ) {
-    e.target.classList.toggle("checked");
-    toggleChecked(e.target.getAttribute("taskId"));
-    if (e.target.classList.contains("checked")) {
-      e.target.children[0].children[0].src = "./Images/checked.png";
-    } else {
-      e.target.children[0].children[0].src = "./Images/unchecked.png";
-    }
+function done(e, id) {
+  let target = e.target;
+  let p = target.parentNode.children[1];
+  p.classList.toggle("done");
+  if (p.classList.contains("done")) {
+    target.src = "./Images/checked.png";
+    selectingTask(id);
+  } else {
+    deSelectingTask(id);
+    target.src = "./Images/unchecked.png";
   }
-});
-
-function toggleChecked(taskId) {
-  for (let i = 0; i < arrayOfTasks.length; i++) {
-    if (arrayOfTasks[i].id == taskId) {
-      arrayOfTasks[i].completed == false
-        ? (arrayOfTasks[i].completed = true)
-        : (arrayOfTasks[i].completed = false);
+}
+function selectingTask(id) {
+  let tasks = getLocalStorage();
+  tasks.map((ele) => {
+    if (ele.id == id) {
+      ele.completed = true;
     }
-  }
-  window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function deSelectingTask(id) {
+  let tasks = getLocalStorage();
+  tasks.map((ele) => {
+    if (ele.id == id) {
+      ele.completed = false;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
